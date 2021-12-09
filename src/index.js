9cf55c545d9897cf2598bc0bbeef9506541e958c
haefneryan <haefner.ryan@gmail.com>
2021-12-09 10:21:50 -0500

    _

--- src/index.js
+++ src/index.js
@@ -5,7 +5,5 @@ import { BrowserRouter as Router} from 'react-router-dom';
 import './index.css';
 import App from './App';
 
-
 ReactDOM.render(<Router basename={process.env.PUBLIC_URL}><App /></Router>, document.getElementById('root')
 );
-
